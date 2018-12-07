0ab71d55e4c3f42609484fb9ed3959c59dfa4587
Flint <flint@iMac1.local>
2018-12-07 08:23:40 -0500

    Added the golden truss

--- logo.py
+++ logo.py
@@ -1,4 +1,6 @@
 import pygame
+
+
 class Logo:
 
     def __init__(self, main_surface):
@@ -12,5 +14,9 @@ class Logo:
     def draw_logo(self):
         pygame.draw.rect(self.main_surface, self.green, (5, 5, 400, 200), 3)
         pygame.draw.rect(self.main_surface, self.green, (10, 10, 390, 190), 0)
+        pygame.draw.polygon(self.main_surface, self.gold, ((145, 100), (265, 100), (205, 20)), 3)
+        pygame.draw.line(self.main_surface, self.gold, (205, 100), (205, 20), 3)
+        pygame.draw.line(self.main_surface, self.gold, (205, 100), (235, 60), 3)
+        pygame.draw.line(self.main_surface, self.gold, (205, 100), (175, 60), 3)
 
         pygame.display.update()
--- unit_ten_warmup.py
+++ unit_ten_warmup.py
@@ -1,10 +1,13 @@
 import pygame
 import sys
+import logo
 from pygame.locals import*
 
 pygame.init()
-main_surface = pygame.display.set_mode((700, 500), 0, 32)
+main_surface = pygame.display.set_mode((500, 250), 0, 32)
 pygame.display.set_caption("SSFS Logo")
+my_logo = logo.Logo(main_surface)
+my_logo.draw_logo()
 
 while True:
     for event in pygame.event.get():
