edf9d34199b79033600eeb1258596f9cc8b1bf68
Flint <flint@iMac1.local>
2018-12-10 09:23:37 -0500

    added the words

--- unit_ten_warmup.py
+++ unit_ten_warmup.py
@@ -8,6 +8,7 @@ main_surface = pygame.display.set_mode((500, 250), 0, 32)
 pygame.display.set_caption("SSFS Logo")
 my_logo = logo.Logo(main_surface)
 my_logo.draw_logo()
+my_logo.draw_words()
 
 while True:
     for event in pygame.event.get():
